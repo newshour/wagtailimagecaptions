a3bb55078daa95dfd950a17c1c7745a3798d1756
Stephan Rohde <srohde@newshour.org>
2023-07-13 14:51:31 -0400

    Adding media URLs

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.conf import settings
 
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
@@ -9,3 +12,6 @@ urlpatterns = [
     path("wagtail/", include(wagtailadmin_urls)),
     path("", include(wagtail_urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
