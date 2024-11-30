a3fa0c798e4c9a261b803bd82593b14d545616e9
Stephan Rohde <srohde@newshour.org>
2024-11-30 09:06:45 -0500

    Registering model for reference index.

--- src/wagtailimagecaptions/apps.py
+++ src/wagtailimagecaptions/apps.py
@@ -6,5 +6,12 @@ class WagtailImageCaptionsConfig(AppConfig):
     name = "wagtailimagecaptions"
 
     def ready(self):
+        # Register CaptionedImage for referencing (usage stats).
+        from wagtail.models.reference_index import ReferenceIndex
+
+        from .models import CaptionedImage
+
+        ReferenceIndex.register_model(CaptionedImage)
+
         # Implicitly connect signal handlers decorated with @receiver.
         from . import signals  # noqa
